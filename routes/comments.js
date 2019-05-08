0b5335a3db67c899998f07528a0186e17cfbb43f
Andrew Zhang <andrew.zhang9799@gmail.com>
2019-05-09 01:40:04 +1000

    fix flash message error upon deleting comments

--- routes/comments.js
+++ routes/comments.js
@@ -78,7 +78,7 @@ router.delete("/:comment_id", middleware.checkCommentOwnership, function(req, re
         if (err) {
             res.redirect("back");
         } else {
-            res.flash("success", "Comment deleted");
+            req.flash("success", "Comment deleted");
             res.redirect("/campgrounds/" +req.params.id);
         }
     });
